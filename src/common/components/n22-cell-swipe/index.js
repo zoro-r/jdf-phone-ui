f1703e34a0cce5498648f3cd1d85e62d8eecc17a
江道丰 <jiangdaofeng@n22.com.cn>
2018-03-19 16:15:35 +0800

    no message

--- src/common/components/n22-cell-swipe/index.js
+++ src/common/components/n22-cell-swipe/index.js
@@ -2,8 +2,10 @@
  * @Author: jdf
  * @Date: 2018-01-06 21:11:10
  * @Last Modified by: jdf
- * @Last Modified time: 2018-03-19 14:35:46
- * @desc 页面布局组件，用于后期扩展键盘事件，滚动事件等
+ * @Last Modified time: 2018-03-19 16:13:16
+ * @desc 滑动删除组件
+ *  main 主列表
+ *  right 右侧滑出按钮列表
  */
 import N22CellSwipe from './n22-cell-swipe'
 export default N22CellSwipe
