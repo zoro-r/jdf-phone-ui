d91d7abd3cc32f56aabfd4f475990969c8f210e8
pro <805841744@qq.com>
2018-05-11 15:15:49 +0800

    update

--- src/pages/demo/N22Zombi.vue
+++ src/pages/demo/N22Zombi.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="demo_canvas" ref="demo_canvas">
+    <!-- 顶部操作区域 -->
     <div class="option">
-      <span class="score">{{score}}分</span>
+      <div class="time item">
+        <span style="right:20px">{{time}}秒</span>
+      </div>
+      <div class="score item">
+        <span style="right:5px">{{score}}分</span>
+      </div>
     </div>
 
+    <!-- 植物 -->
+    <div class="plant">
+      <!-- 种子 -->
+      <img :style="{'opacity': palntIndex == 1 ? '1' : '0'}" class="seed" src="./../../../static/images/plants/plant_01/plant_01.png" alt="">
+      <img :style="{'opacity': palntIndex == 2 ? '1' : '0'}" class="seedling" src="./../../../static/images/plants/plant_01/plant_02.png" alt="">
+      <img :style="{'opacity': palntIndex == 3 ? '1' : '0'}"  class="seedling" src="./../../../static/images/plants/plant_01/plant_03.png" alt="">
+      <img :style="{'opacity': palntIndex > 3 ? '1' : '0'}"  class="seedling" src="./../../../static/images/plants/plant_01/plant_04.png" alt="">
+    </div>
   </div>
 </template>
 
@@ -12,7 +26,9 @@ export default {
   name: 'n22Canvas',
   data () {
     return {
-      score: 0
+      score: 0,
+      time: 60,
+      palntIndex: 1
     }
   },
   methods: {
@@ -31,6 +47,7 @@ export default {
         this.score++
       }, 1100)
     },
+
     // 创造阳光
     createSun () {
       let position = this.getPosition()
@@ -39,15 +56,16 @@ export default {
       image.className += 'sun_img'
       image.style.right = position.right + 'px'
       image.style.top = 50 + 'px'
-      image.style.transition = 'all ' + 10 + 's linear'
+      image.style.transition = 'all ' + 8 + 's linear'
       image.onload = () => {
         this.$refs.demo_canvas.appendChild(image)
       }
       image.addEventListener('touchstart', this.getSun)
       setTimeout(() => {
-        image.style.top = this.screenHeight + 10 + 'px'
+        image.style.top = this.screenHeight + 50 + 'px'
       }, 200)
     },
+
     // 获取位置
     getPosition () {
       let right = Math.random() * this.screenWidth
@@ -58,14 +76,50 @@ export default {
       return {
         right, top
       }
+    },
+
+    // 倒计时
+    timeStart () {
+      let startTime = new Date()
+      let allTime = this.time
+      setInterval(e => {
+        let cha = new Date() - startTime
+        this.time = allTime - parseInt(cha / 1000)
+      }, 1000)
+    },
+
+    // 开始游戏
+    startGame () {
+      // 计时开始
+      this.timeStart()
+      // 创造阳光
+      setInterval(e => {
+        this.palntIndex ++
+        if (this.palntIndex == 5) {
+          this.palntIndex = 1
+        }
+        this.createSun()
+      }, 1000)
+    },
+
+    // 游戏结束
+    gameOver () {
+      // @todu 清空所有计时器 清空页面数据
     }
   },
-  mounted () {
+  beforeRouteEnter: (to, from, next) => {
     // 禁止页面滚动
     window.utils.ui.disabledBodyScroll(true)
-    setInterval(e => {
-      this.createSun()
-    }, 200)
+    document.body.style.top = '0px'
+    next()
+  },
+  beforeRouteLeave: (to, from, next) => {
+    // 恢复初始状态
+    window.utils.ui.disabledBodyScroll(false)
+    next()
+  },
+  mounted () {
+    this.startGame()
   }
 }
 </script>
@@ -77,13 +131,57 @@ export default {
   background-size: 100% 100%;
   // 操作区域
   .option{
-    position: relative;
-    .score{
-      font-size: $font-md;
-      position: absolute;
+    position: absolute;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    justify-content: space-between;
+    .item{
+      font-size: 2rem;
       color: $primary-color;
-      right: 100px;
-      top: 15px;
+      width: 150px;
+      height: 50px;
+      border-radius: 3px;
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      position: relative;
+      display: flex;
+      align-items: center;
+      span{
+        position: absolute;
+      }
+    }
+    .score{
+      background-image: url('./../../../static/images/plants/score.png');
+    }
+    .time{
+      background-image: url('./../../../static/images/plants/time.jpg');
+    }
+  }
+  .plant{
+    width: 100px;
+    height: 100px;
+    background-size: 100%;
+    position: absolute;
+    pointer-events: none;
+    z-index: 0;
+    top: 200px;
+    // border: 1px solid black;
+    img{
+      transition: all .3s linear;
+    }
+    .seed{
+      width: 40px;
+      position: absolute;
+      bottom: 0px;
+      left: 12px;
+    }
+    // 幼苗
+    .seedling{
+      width: 80px;
+      position: absolute;
+      bottom: 0px;
     }
   }
   .sun_img {
